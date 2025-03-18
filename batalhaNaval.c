21200ad30c62f197166f7bce3416c3fd4e3aaceb
Malu9862 <maria.luisa997425@gmail.com>
2025-03-18 20:42:51 +0000

    Navio Horizontal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,3 +5,11 @@
 int main() {
     // Posicionamento dos Navios
     int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO] = {0}; // Inicializa o tabuleiro com 0 (água)
+
+        // Navio Horizontal
+        int linha_horizontal = 2, coluna_horizontal = 3; 
+        for (int i = 0; i < 3; i++) { 
+            if (coluna_horizontal + i < TAM_TABULEIRO) {
+                tabuleiro[linha_horizontal][coluna_horizontal + i] = 3; // Marca a posição do navio
+            }
+        }
